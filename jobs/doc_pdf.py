39c72a9ec27897c5c2a0532c2fed98dbaee8781e
alex <risskail@yandex.ru>
2021-06-04 10:42:56 +0500

    'default_app'

--- jobs/doc_pdf.py
+++ jobs/doc_pdf.py
@@ -4,70 +4,76 @@ import base64
 from jobs import sql
 import os
 
-def get_pdf_for_doc():
+class Load_update_pdf()
 
-	rezult_table = sql.session.query(sql.TableObject).all()
-	for doc in rezult_table:
+	def get_pdf_for_doc(self):
 
-		new_folder = '/var/www/share/static/pdf/{}/'.format(doc.number)
-		if os.path.exists(new_folder) == False:
-			os.mkdir(new_folder)
-		else:
-			# тут удаляю все файлы
-			#нужно переделать!!!
-			list_file = os.listdir(new_folder)
-			for file in list_file:
-				if os.path.isfile(new_folder + file):
+		rezult_table = sql.session.query(sql.TableObject).all()
+		for doc in rezult_table:
+
+			new_folder = '/var/www/share/static/pdf/{}/'.format(doc.number)
+			if os.path.exists(new_folder) == False:
+				os.mkdir(new_folder)
+			else:
+				# тут удаляю все файлы
+				#нужно переделать!!!
+				list_file = os.listdir(new_folder)
+				for file in list_file:
+					if os.path.isfile(new_folder + file):
+						os.remove(new_folder + file)
+
+			payload = json.dumps({'NumberDoc':doc.number,'DocType':'1'})
+			
+			r = requests.post('http://192.168.111.204/poradom_base/hs/GetTask/get_pdf',auth=("login","password"),data=payload)
+			data = r.content
+
+			try:
+				dict_files = json.loads(data)
+				for file_ in dict_files:
+					
+					if file_['pdf'] != None:
+						new_file_name = '/var/www/share/static/pdf/{0}/{1}'.format(doc.number,file_['name'])
+						file = open(new_file_name,'wb')
+						file.write(base64.b64decode(file_['pdf']))
+						file.close()
+					else:
+						print('Error')
+			except:
+				print('Error {}'.format(doc.number))
+
+	def get_pdf_for_dop(self):
+		rezult_table = sql.session.query(sql.TableObject).all()
+		for doc in rezult_table:
+		
+			new_folder = '/var/www/share/static/pdf/{}/dop/'.format(doc.number)
+			if os.path.exists(new_folder) == False:
+				os.mkdir(new_folder)
+			else:
+				# тут удаляю все файлы
+				#нужно переделать!!!
+				list_file = os.listdir(new_folder)
+				for file in list_file:
 					os.remove(new_folder + file)
 
-		payload = json.dumps({'NumberDoc':doc.number,'DocType':'1'})
-		
-		r = requests.post('http://192.168.111.204/poradom_base/hs/GetTask/get_pdf',auth=("login","password"),data=payload)
-		data = r.content
+			payload = json.dumps({'NumberDoc':doc.number,'DocType':'1'})
+			
+			r = requests.post('http://192.168.111.204/poradom_base/hs/GetTask/get_pdf_dop',auth=("login","password"),data=payload)
+			data = r.content
 
-		try:
-			dict_files = json.loads(data)
-			for file_ in dict_files:
-				
-				if file_['pdf'] != None:
-					new_file_name = '/var/www/share/static/pdf/{0}/{1}'.format(doc.number,file_['name'])
-					file = open(new_file_name,'wb')
-					file.write(base64.b64decode(file_['pdf']))
-					file.close()
-				else:
-					print('Error')
-		except:
-			print('Error {}'.format(doc.number))
-
-def get_pdf_for_dop():
-	rezult_table = sql.session.query(sql.TableObject).all()
-	for doc in rezult_table:
+			try:
+				dict_files = json.loads(data)
+				for file_ in dict_files:
+					
+					if file_['pdf'] != None:
+						new_file_name = '/var/www/share/static/pdf/{0}/dop/{1}'.format(doc.number,file_['name'])
+						file = open(new_file_name,'wb')
+						file.write(base64.b64decode(file_['pdf']))
+						file.close()
+					else:
+						print('Error')
+			except:
+				print('Error {}'.format(doc.number))
 	
-		new_folder = '/var/www/share/static/pdf/{}/dop/'.format(doc.number)
-		if os.path.exists(new_folder) == False:
-			os.mkdir(new_folder)
-		else:
-			# тут удаляю все файлы
-			#нужно переделать!!!
-			list_file = os.listdir(new_folder)
-			for file in list_file:
-				os.remove(new_folder + file)
-
-		payload = json.dumps({'NumberDoc':doc.number,'DocType':'1'})
-		
-		r = requests.post('http://192.168.111.204/poradom_base/hs/GetTask/get_pdf_dop',auth=("login","password"),data=payload)
-		data = r.content
-
-		try:
-			dict_files = json.loads(data)
-			for file_ in dict_files:
-				
-				if file_['pdf'] != None:
-					new_file_name = '/var/www/share/static/pdf/{0}/dop/{1}'.format(doc.number,file_['name'])
-					file = open(new_file_name,'wb')
-					file.write(base64.b64decode(file_['pdf']))
-					file.close()
-				else:
-					print('Error')
-		except:
-			print('Error {}'.format(doc.number))
+	def main(self):
+		self.get_pdf_for_doc()
+		self.get_pdf_for_dop()
--- run_jobs.py
+++ run_jobs.py
@@ -1,18 +1,18 @@
 from jobs import info_doc, image_stage, doc_pdf, start_settings
 import time
 
+start_settings.insert_default_settings()
+
+new_Doc 	= info_doc.Doc_and_stage()
+new_Stage 	= image_stage.Load_update_image()
+newPDF 		= doc_pdf.Load_update_pdf()
+
+
 def main():
 	
-	start_settings.insert_default_settings()
-	new_Doc = info_doc.Doc_and_stage()
 	new_Doc.main()
-	
-	image_stage.delete_image()
-	image_stage.load_images()
-	image_stage.update_sum_image_in_stage()
-	image_stage.upload_image_from_srv()
-	doc_pdf.get_pdf_for_doc()
-	doc_pdf.get_pdf_for_dop()
+	new_Stage.main()
+	newPDF.main()
 
 #main()
 #while True:
